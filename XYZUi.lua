8e46994e3cdd294e806b5b032549dafd5832a15e
Tran Van Bao <133487627+TranVanBao1411@users.noreply.github.com>
2023-10-11 14:47:03 +0700

    Add files via upload

--- XYZUi.lua
+++ XYZUi.lua
@@ -11,17 +11,6 @@ local titlefunc = {}
 local UIConfig = {
   Bind = Enum.KeyCode.RightControl
 }
-local chars = "yeuemngoclinh"
-local length = 20
-local randomString = ""
-math.randomseed(os.time())
-charTable = {}
-for c in chars:gmatch "." do
-    table.insert(charTable, c)
-end
-for i = 1, length do
-    randomString = randomString .. charTable[math.random(1, #charTable)]
-end
 
 _G.Color = Color3.fromRGB(61, 224, 11)
 function CircleClick(Button, X, Y)
