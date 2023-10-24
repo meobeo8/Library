74a7dc03522cb3eb6837bc1170a526aed2d9b06f
Tran Van Bao <133487627+TranVanBao1411@users.noreply.github.com>
2023-10-24 07:16:12 +0700

    Add files via upload

--- XYZUi.lua
+++ XYZUi.lua
@@ -564,13 +564,13 @@ spawn(function()
       {
         Size = UDim2.new(game.Players.LocalPlayer.Character.Energy.Value/game.Players.LocalPlayer.Character.Energy.MaxValue, 0, 1, 0)} -- UDim2.new(0, 128, 0, 25)
     ):Play()
-
-    HealthText.Text = "Health : "..game.Workspace.LocalPlayer.Character.Humanoid.Health.."/"..game.Workspace.LocalPlayer.Character.Humanoid.MaxHealth
+   local aiosimi = game.Workspace.LocalPlayer.Character.Humanoid
+    HealthText.Text = "Health : "..aiosimi.Health.."/"..aiosimi.MaxHealth
     TweenService:Create(
       LineHealth,
       TweenInfo.new(0.4, Enum.EasingStyle.Quad, Enum.EasingDirection.Out),
       {
-        Size = UDim2.new(game.Players.LocalPlayer.Character.Humanoid.Health/game.Players.LocalPlayer.Character.Humanoid.MaxHealth, 0, 0, 5)} -- UDim2.new(0, 128, 0, 25)
+        Size = UDim2.new(aiosimi.Humanoid.Health/aiosimi.MaxHealth, 0, 0, 5)} -- UDim2.new(0, 128, 0, 25)
     ):Play()
     end)
   end
