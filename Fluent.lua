7f67eebf3c8a53a41f427629032fc2a27fbc8ba3
Tran Van Bao <133487627+TranVanBao1411@users.noreply.github.com>
2024-05-30 11:54:35 +0700

    Add files via upload

--- Fluent.lua
+++ Fluent.lua
@@ -39,16 +39,7 @@ ThunderImageUI.BackgroundTransparency = 0.1
 ThunderImageUI.BorderSizePixel = 0
 ThunderImageUI.Position = UDim2.new(0.0, 0, 0.0, 0)
 ThunderImageUI.Size = UDim2.new(0, 45, 0, 45)
-ThunderImageUI.Image = "rbxassetid://17612064986"
-
-function CloseUi()
-    for _, v in ipairs(game.CoreGui:GetChildren()) do
-        if v.Name == "ScreenGui" or v.Name == "TrueFalseUi" or v.Name == "hi22" then
-            v:Destroy()
-        end
-    end
-end
-
+ThunderImageUI.Image = "rbxassetid://17664629557"
 
 local L_1_, L_2_ = {
    {
@@ -437,8 +428,6 @@ local L_1_, L_2_ = {
       }
    }
 }
-
-
 
 local L_3_ = {
    function()
@@ -4386,28 +4375,40 @@ end,
 local L_1139_, L_1140_, L_1141_, L_1142_, L_1143_ = L_2_(51)
 return {
 Name = 'Darker',
-Accent = Color3.fromRGB(232, 181, 138),
-AcrylicMain = Color3.fromRGB(232, 181, 138),
-AcrylicBorder = Color3.fromRGB(25, 25, 25),
-AcrylicGradient = ColorSequence.new(Color3.fromRGB(25, 25, 25), Color3.fromRGB(15, 15, 15)),
-AcrylicNoise = 1,
-TitleBarLine = Color3.fromRGB(232, 181, 138),
-Tab = Color3.fromRGB(25, 25, 25),
-Element = Color3.fromRGB(25, 25, 25),
-ElementBorder = Color3.fromRGB(25, 25, 25),
-InElementBorder = Color3.fromRGB(25, 25, 25),
-ElementTransparency = 0,
-DropdownFrame = Color3.fromRGB(120, 120, 120),
-DropdownHolder = Color3.fromRGB(35, 35, 35),
-DropdownBorder = Color3.fromRGB(25, 25, 25),
-Dialog = Color3.fromRGB(35, 35, 35),
-DialogHolder = Color3.fromRGB(25, 25, 25),
-DialogHolderLine = Color3.fromRGB(20, 20, 20),
-DialogButton = Color3.fromRGB(35, 35, 35),
-DialogButtonBorder = Color3.fromRGB(55, 55, 55),
-DialogBorder = Color3.fromRGB(50, 50, 50),
-DialogInput = Color3.fromRGB(45, 45, 45),
-DialogInputLine = Color3.fromRGB(120, 120, 120)
+Accent = Color3.fromRGB(180, 55, 90),
+AcrylicMain = Color3.fromRGB(40, 40, 40),
+AcrylicBorder = Color3.fromRGB(130, 90, 110),
+AcrylicGradient = ColorSequence.new(Color3.fromRGB(190, 60, 135), Color3.fromRGB(165, 50, 70)),
+AcrylicNoise = 0.92,
+TitleBarLine = Color3.fromRGB(140, 85, 105),
+Tab = Color3.fromRGB(180, 140, 160),
+Element = Color3.fromRGB(200, 120, 170),
+ElementBorder = Color3.fromRGB(110, 70, 85),
+InElementBorder = Color3.fromRGB(120, 90, 90),
+ElementTransparency = 0.86,
+ToggleSlider = Color3.fromRGB(200, 120, 170),
+ToggleToggled = Color3.fromRGB(0, 0, 0),
+SliderRail = Color3.fromRGB(200, 120, 170),
+DropdownFrame = Color3.fromRGB(200, 160, 180),
+DropdownHolder = Color3.fromRGB(120, 50, 75),
+DropdownBorder = Color3.fromRGB(90, 40, 55),
+DropdownOption = Color3.fromRGB(200, 120, 170),
+Keybind = Color3.fromRGB(200, 120, 170),
+Input = Color3.fromRGB(200, 120, 170),
+InputFocused = Color3.fromRGB(20, 10, 30),
+InputIndicator = Color3.fromRGB(170, 150, 190),
+Dialog = Color3.fromRGB(120, 50, 75),
+DialogHolder = Color3.fromRGB(95, 40, 60),
+DialogHolderLine = Color3.fromRGB(90, 35, 55),
+DialogButton = Color3.fromRGB(120, 50, 75),
+DialogButtonBorder = Color3.fromRGB(155, 90, 115),
+DialogBorder = Color3.fromRGB(100, 70, 90),
+DialogInput = Color3.fromRGB(135, 55, 80),
+DialogInputLine = Color3.fromRGB(190, 160, 180),
+Text = Color3.fromRGB(240, 240, 240),
+SubText = Color3.fromRGB(170, 170, 170),
+Hover = Color3.fromRGB(200, 120, 170),
+HoverChange = 0.04
 }
 end,
 [52] = function()
