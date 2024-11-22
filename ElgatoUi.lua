3454f9bdd420aaecd3ed7264aabdecebcc8280a8
Tran Van Bao <133487627+meobeo8@users.noreply.github.com>
2024-11-22 18:11:12 +0700

    Update ElgatoUi.lua

--- ElgatoUi.lua
+++ ElgatoUi.lua
@@ -182,7 +182,7 @@ local redzlib = {
     Options = {},
     Flags = {},
     Tabs = {},
-    Icons = loadstring(game:HttpGet("https://raw.githubusercontent.com/realredz/RedzLibV5/refs/heads/main/Icons.Lua"))()
+    Icons = loadstring(game:HttpGet("https://raw.githubusercontent.com/meobeo8/Library/refs/heads/retard/Icons.lua.txt"))()
 }
 
 local ViewportSize = workspace.CurrentCamera.ViewportSize
